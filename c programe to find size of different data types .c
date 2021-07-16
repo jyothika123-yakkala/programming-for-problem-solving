9afeea2a2c24f12d0d1cc72f6c149bb6c57dd859
jyothika123-yakkala <85831624+jyothika123-yakkala@users.noreply.github.com>
2021-07-16 17:10:39 +0530

    Add files via upload

---
+++ c programe to find size of different data types .c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int integertype;
+	char chartype;
+	float floattype;
+	double doubletype;
+	double longdouble;
+	printf("size of int is: %d\n",sizeof(int));
+	printf("size of char is: %d\n",sizeof (char));
+	printf("size of float is: %d\n",sizeof(float));
+	printf("size of double is:%d\n",sizeof (double));
+	printf("size of longdouble is: %d\n",sizeof(long double));
+	return(0);
+}
