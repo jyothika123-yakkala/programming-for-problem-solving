37399e243648f9ee783ba791a21046e253b1cf53
jyothika123-yakkala <85831624+jyothika123-yakkala@users.noreply.github.com>
2021-07-16 16:34:28 +0530

    Add files via upload

---
+++ characters in a reverse way.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	char char1 = 'p';
+	char char2 = 'p';
+	char char3 = 's';
+	printf(" the reverse of %c%c%c is %c%c%c\n",
+	char1,char2,char3,
+	char3,char2,char1);
+	return(0);
+}
