1edcd30089a68a6e746a4b5dac4c7a5f7ec02890
jyothika123-yakkala <85831624+jyothika123-yakkala@users.noreply.github.com>
2021-07-18 21:58:38 +0530

    Add files via upload

---
+++ 59.checking for given character is alphabet ,using if else.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	char ch;
+	printf("enter a character:");
+	scanf("%c",&ch);
+	if((ch>='a'&& ch<='z')||(ch>='A' && ch<='z'))
+	printf("\nthe entered character %c is an alphabet",ch);
+	else
+	printf("\n the entered character %c is not alphabet",ch);
+}
