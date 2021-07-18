75c8aa7013571a27f2997440f39dbc3cfb8dad98
jyothika123-yakkala <85831624+jyothika123-yakkala@users.noreply.github.com>
2021-07-18 21:53:23 +0530

    Add files via upload

---
+++ 55.checking if num is odd or even.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int num;
+	printf("\nenter a number:");
+	scanf("%d",&num);
+	if(num%2 ==0)
+	printf("\n num is even");
+	else
+	printf("\n num is odd");
+	return(0);
+}
