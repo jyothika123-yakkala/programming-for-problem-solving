779bcd0f13c4514fbd629e50b7ea8b526627e4f9
jyothika123-yakkala <85831624+jyothika123-yakkala@users.noreply.github.com>
2021-07-18 22:10:54 +0530

    Add files via upload

---
+++ 64.printing days of a week using switch case.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+int main()
+{
+	int num
+	num num1 = 1;
+	num num2 = 2;
+	num num3 = 3;
+	num num4 = 4;
+	num num5 = 5;
+	num num6 = 6;
+	num num7 = 7;
+	printf("\n enter num:");
+	scanf("%d",&num);
+	if(num=num1)
+	printf("\n monday");
+	break;
+	if(num = num2);
+	printf("\n tuesday");
+	break;
+	if(num= num3);
+	printf("\n wednesday");
+	break;
+	if(num = num4);
+	printf("\n thursday");
+	break;
+	if(num = num5)
+	printf("\n friday");
+	break;
+	if(num = num6)
+	printf("\n saturday");
+	break;
+	if(num = num7)
+	printf("\n sunday");
+	break;
+	return(0);
+}
