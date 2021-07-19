2f12dff47d2ee0ba9175688dd5eb26ddb2546c97
jyothika123-yakkala <85831624+jyothika123-yakkala@users.noreply.github.com>
2021-07-19 21:30:06 +0530

    Add files via upload

---
+++ 37. program to find lsb of a number.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int num;
+    printf("Enter any number: ");
+    scanf("%d", &num);
+
+    /* If (num & 1) evaluates to 1 */
+    if(num & 1)
+        printf("LSB of %d is set (1).", num);
+    else
+        printf("LSB of %d is unset (0).", num);
+
+    return 0;
+}
