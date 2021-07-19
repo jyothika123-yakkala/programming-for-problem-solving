f24cf0b6d7e9c2cb682023df1318235f6a377b01
jyothika123-yakkala <85831624+jyothika123-yakkala@users.noreply.github.com>
2021-07-19 21:54:51 +0530

    Add files via upload

---
+++ 33. bitwise operators.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int a = 12, b = 25;
+    printf("Output = %d\n", a&b);
+    printf("Output = %d\n", a|b);
+	printf("Output = %d\n", a^b);
+	return(0);
+}
+	
+    
