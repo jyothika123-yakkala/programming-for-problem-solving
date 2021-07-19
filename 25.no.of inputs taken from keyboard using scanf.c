4bdf36c8c64f04c25d0b9d30fca7fe40711f5341
jyothika123-yakkala <85831624+jyothika123-yakkala@users.noreply.github.com>
2021-07-19 21:34:16 +0530

    Add files via upload

---
+++ 25.no.of inputs taken from keyboard using scanf.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	int num1,num2,num3,n;
+	printf("\nenter the three numbers:");
+	n=scanf("%d\n%d\n%d",&num1,&num2,&num3);
+	
+	printf("\n number of inputs scanf statement took here is %d",n);
+	return 0;
+}
