df00c0bdb2075b8ffff86e6107a1b18f255743f3
jyothika123-yakkala <85831624+jyothika123-yakkala@users.noreply.github.com>
2021-07-20 07:57:01 +0530

    Add files via upload

---
+++ 52.program to check num is posi,negi,or zero.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	int num;
+	printf("\n enter a number:");
+	scanf("%d",&num);
+	if(num>0)
+	printf("\n number is positive");
+	if(num<0)
+	printf("\n num is negitive");
+	if(num = 0)
+	printf("\n num is zero");
+	return(0);
+}
